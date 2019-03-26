a0540fc42211becb8b2df9557f95b77a392c4363
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-03-26 08:19:00 +0100

    only DCT with notary; removed authz for registries.

--- src/main/main.go
+++ src/main/main.go
@@ -18,7 +18,7 @@ const (
 
 var (
 	flDockerHost         = flag.String("host", defaultDockerHost, "Specifies the host where docker daemon is running")
-	authorizedRegistries stringslice
+	authorizedNotary     = flag.String("notary", "", "Specifies the authorized image notary")
 	Version              string
 	Build                string
 )
@@ -26,21 +26,11 @@ var (
 func main() {
 
 	log.Println("Plugin Version:", Version, "Build: ", Build)
-
-	// Fetch the registry cmd line options
-	flag.Var(&authorizedRegistries, "registry", "Specifies the authorized image registries")
 	flag.Parse()
-
-	// Convert authorized registries into a map for efficient lookup
-	registries := make(map[string]bool)
-	for _, registry := range authorizedRegistries {
-		log.Println("Authorized registry:", registry)
-		registries[registry] = true
-	}
-	log.Println("No. of authorized registries: ", len(registries))
+	log.Println("Authorized notary: ", authorizedNotary)
 
 	// Create image authorization plugin
-	plugin, err := newPlugin(*flDockerHost, registries)
+	plugin, err := newPlugin(*flDockerHost, *authorizedNotary)
 	if err != nil {
 		log.Fatal(err)
 	}
